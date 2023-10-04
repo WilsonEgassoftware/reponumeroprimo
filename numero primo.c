5b2df31923b1dacf140ada375b1c81c69e158155
WilsonEgassoftware <146997073+WilsonEgassoftware@users.noreply.github.com>
2023-10-04 16:41:17 -0500

    Create numero primo.c

---
+++ numero primo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    int numero ,i;
+    int divisor=2;
+
+
+    printf("Ingrese un numero :");
+    scanf("%d", &numero);
+       
+    if (numero > 2){
+       (divisor=numero/2);
+      printf ("El numero es primo");
+    }
+    else{ 
+        (numero == 0);
+        (divisor/2);
+        printf("El numero no es primo");
+    }
+  return 0;
+
+}
